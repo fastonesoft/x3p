36a51d1bc6af3d30024f72da55080bfcbb3d32bc
fastonesoft <2347632@qq.com>
2019-10-10 09:02:26 +0800

    login.comment

--- web/src/components/dev-login.vue
+++ web/src/components/dev-login.vue
@@ -1,74 +1,138 @@
 <template>
-    <div class="login">
-        <div class="body">
-            <div id="cloud1" class="cloud"></div>
-            <div id="cloud2" class="cloud"></div>
-            <div class="bg">
-                <slot name="passLogin"></slot>
-            </div>
-        </div>
+    <div class="form">
+        <i-col class="title">{{title}}</i-col>
+        <Form ref="form" :model="form" :rules="rule">
+            <FormItem prop="id">
+                <Input type="text" prefix="md-contact" v-model="form.id" size="large" :maxlength="20"
+                       placeholder="输入帐号，5-20个字符"/>
+            </FormItem>
+            <FormItem prop="pass">
+                <Input type="password" prefix="ios-eye" v-model="form.pass" size="large" :maxlength="20"
+                       placeholder="输入密码，6-20个字符"/>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" size="large" @click="loginClick('form')" long
+                        style="background-color: #378CBE;">登录
+                </Button>
+            </FormItem>
+        </Form>
     </div>
 </template>
 
 <script>
+    import xcon from '../libs/xcon'
+
     export default {
         name: "dev-login",
 
-        mounted() {
-            let that = this;
-            window.onresize = function () {
-                // 登录页面才要进行位置计算
-                if (that.$route.path !== '/vlogin') return;
-
-                let awid = document.body.clientWidth;
-                let ahei = document.body.clientHeight;
-                let left1 = awid / 4 * Math.random();
-                let top1 = ahei / 4 * Math.random();
-                let left2 = awid / 2 + awid / 4 * Math.random();
-                let top2 = ahei / 4 * Math.random();
-                document.getElementById('cloud1').style.left = left1 + 'px';
-                document.getElementById('cloud1').style.top = top1 + 'px';
-                document.getElementById('cloud2').style.left = left2 + 'px';
-                document.getElementById('cloud2').style.top = top2 + 'px';
-            };
-            window.onresize();
+        props: {
+            title: String,
         },
+
+        data() {
+            return {
+                token: '',
+                form: {
+                    id: '',
+                    pass: '',
+                },
+                rule: {
+                    id: [
+                        {
+                            required: true, message: '请输入用户账号', trigger: 'change'
+                        },
+                        {
+                            type: 'string',
+                            min: 5,
+                            max: 20,
+                            message: '最小5位，最大20位',
+                            trigger: 'change'
+                        },
+                        {
+                            pattern: /^\w+$/,
+                            message: '必须是字母、数字或者下划线',
+                            trigger: 'change'
+                        },
+                    ],
+                    pass: [
+                        {
+                            required: true, message: '请输入用户密码', trigger: 'change'
+                        },
+                        {
+                            type: 'string',
+                            min: 6,
+                            max: 20,
+                            message: '密码长度最小6位，最大20位',
+                            trigger: 'change'
+                        },
+                        {
+                            pattern: /^[\\.\w]+$/,
+                            message: '必须是字母、数字或者下划线',
+                            trigger: 'change'
+                        },
+                    ]
+                },
+            }
+        },
+        methods:
+            {
+                loginClick(name) {
+                    this.$refs[name].validate((valid) => {
+                        if (valid) {
+                            // 提交this.form服务器端认证
+                            xcon.posts('/home/login', {
+                                id: this.form.id,
+                                pass: xcon.md5(this.form.pass),
+                                token: xcon.md5(this.token + xcon.md5(this.form.pass))
+                            })
+                                .then(res => {
+                                    // 记录用户信息
+                                    this.$store.commit('userSet', res.user);
+                                    this.$store.commit('typeSet', res.types);
+                                    this.$store.commit('menuSet', res.menus);
+                                    this.$store.commit('timeSet', new Date().toLocaleString());
+                                    // 写入本地
+                                    xcon.stateWrite(this.$store.state);
+                                    this.$router.replace('/vhome');
+                                })
+                                .catch(error => {
+                                    this.$Message.error(error);
+                                });
+                        } else {
+                            this.$Message.error('登录信息无法通过验证，请重新输入');
+                        }
+                    })
+
+                }
+            },
+        created() {
+            xcon.stateClear();
+            xcon.gets('/home/token')
+                .then(res => {
+                    // token
+                    this.token = res;
+                })
+                .catch(error => {
+                    this.$Message.error(error);
+                });
+        }
     }
 </script>
 
-<style>
-    .login {
-        height: 100%;
-        background: #1c77ac;
-        overflow: hidden;
+<style scoped>
+    .form {
+        margin-left: 206px;
+        width: 380px;
+        height: 322px;
+        padding: 30px 40px;
     }
 
-    .body {
-        width: 100%;
-        height: 100%;
-        background: url(../assets/login_light.png) no-repeat center center;
-        position: relative;
-        overflow: hidden;
+    .title {
+        margin-bottom: 30px;
+        text-align: center;
+        letter-spacing: 5px;
+        text-shadow: 1px 1px 3px #666;
+        color: #197BAB;
+        font-size: 24px;
     }
-
-    .cloud {
-        top: 0px;
-        left: 0px;
-        width: 405px;
-        height: 165px;
-        position: absolute;
-        background: url(../assets/login_cloud.png) no-repeat;
-        z-index: 1;
-        opacity: 0.5;
-    }
-
-    .bg {
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        position: absolute;
-        background: url(../assets/login_bg.png) no-repeat center center;
-        z-index: 2;
-    }
-
 </style>
--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,129 +1,86 @@
 <template>
-    <dev-login>
-        <div class="login-code" slot="passLogin">
-            <div class="login-form">
-                <i-col class="login-form-title">校务在线管理系统</i-col>
-                <Form ref="form" :model="form" :rules="rule">
-                    <FormItem prop="id">
-                        <Input type="text" prefix="md-contact" v-model="form.id" size="large" :maxlength="20"
-                               placeholder="输入帐号，5-20个字符"/>
-                    </FormItem>
-                    <FormItem prop="pass">
-                        <Input type="password" prefix="ios-eye" v-model="form.pass" size="large" :maxlength="20"
-                               placeholder="输入密码，6-20个字符"/>
-                    </FormItem>
-                    <FormItem>
-                        <Button type="primary" size="large" @click="loginClick('form')" long
-                                style="background-color: #378CBE;">登录
-                        </Button>
-                    </FormItem>
-                </Form>
+    <div class="login">
+        <div class="light">
+            <div id="cloud1" class="cloud"></div>
+            <div id="cloud2" class="cloud"></div>
+            <div class="bg">
+                <div class="modal">
+                    <dev-login :title="loginTitle"></dev-login>
+                </div>
             </div>
         </div>
-    </dev-login>
+    </div>
 </template>
 
 <script>
-    import devLogin from '../components/dev-login'
-    import xcon from '../libs/xcon'
+    import DevLogin from '../components/dev-login'
 
     export default {
         name: "login",
-        components: {devLogin},
+        components: {DevLogin},
+
         data() {
             return {
-                token: '',
-                form: {
-                    id: '',
-                    pass: '',
-                },
-                rule: {
-                    id: [
-                        {
-                            required: true, message: '请输入用户账号', trigger: 'change'
-                        },
-                        {
-                            type: 'string',
-                            min: 5,
-                            max: 20,
-                            message: '最小5位，最大20位',
-                            trigger: 'change'
-                        },
-                        {
-                            pattern: /^\w+$/,
-                            message: '必须是字母、数字或者下划线',
-                            trigger: 'change'
-                        },
-                    ],
-                    pass: [
-                        {
-                            required: true, message: '请输入用户密码', trigger: 'change'
-                        },
-                        {
-                            type: 'string',
-                            min: 6,
-                            max: 20,
-                            message: '密码长度最小6位，最大20位',
-                            trigger: 'change'
-                        },
-                        {
-                            pattern: /^[\\.\w]+$/,
-                            message: '必须是字母、数字或者下划线',
-                            trigger: 'change'
-                        },
-                    ]
-                },
+                loginTitle: '校务在线管理系统',
             }
         },
-        methods:
-            {
-                loginClick(name) {
-                    this.$refs[name].validate((valid) => {
-                        if (valid) {
-                            // 提交this.form服务器端认证
-                            xcon.posts('/home/login', {
-                                id: this.form.id,
-                                pass: xcon.md5(this.form.pass),
-                                token: xcon.md5(this.token + xcon.md5(this.form.pass))
-                            })
-                                .then(res => {
-                                    // 记录用户信息
-                                    this.$store.commit('userSet', res.user);
-                                    this.$store.commit('typeSet', res.types);
-                                    this.$store.commit('menuSet', res.menus);
-                                    this.$store.commit('timeSet', new Date().toLocaleString());
-                                    // 写入本地
-                                    xcon.stateWrite(this.$store.state);
-                                    this.$router.replace('/vhome');
-                                })
-                                .catch(error => {
-                                    this.$Message.error(error);
-                                });
-                        } else {
-                            this.$Message.error('登录信息无法通过验证，请重新输入');
-                        }
-                    })
 
-                }
-            },
-        created() {
-            xcon.stateClear();
-            xcon.gets('/home/token')
-                .then(res => {
-                    // token
-                    this.token = res;
-                })
-                .catch(error => {
-                    this.$Message.error(error);
-                });
-        }
+        mounted() {
+            let that = this;
+            window.onresize = function () {
+                // 登录页面才要进行位置计算
+                if (that.$route.path !== '/vlogin') return;
+
+                let awid = document.body.clientWidth;
+                let ahei = document.body.clientHeight;
+                let left1 = awid / 4 * Math.random();
+                let top1 = ahei / 4 * Math.random();
+                let left2 = awid / 2 + awid / 4 * Math.random();
+                let top2 = ahei / 4 * Math.random();
+                document.getElementById('cloud1').style.left = left1 + 'px';
+                document.getElementById('cloud1').style.top = top1 + 'px';
+                document.getElementById('cloud2').style.left = left2 + 'px';
+                document.getElementById('cloud2').style.top = top2 + 'px';
+            };
+            window.onresize();
+        },
     }
 </script>
 
 <style scoped>
+    .login {
+        height: 100%;
+        background: #1c77ac;
+        overflow: hidden;
+    }
 
+    .light {
+        height: 100%;
+        background: url(../assets/login_light.png) no-repeat center center;
+        position: relative;
+        overflow: hidden;
+    }
 
-    .login-code {
+    .cloud {
+        top: 0px;
+        left: 0px;
+        width: 405px;
+        height: 165px;
+        position: absolute;
+        background: url(../assets/login_cloud.png) no-repeat;
+        z-index: 1;
+        opacity: 0.5;
+    }
+
+    .bg {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background: url(../assets/login_bg.png) no-repeat center center;
+        z-index: 2;
+    }
+
+    .modal {
         position: fixed;
         left: 50%;
         top: 50%;
@@ -131,25 +88,8 @@
         height: 322px;
         margin-left: -293px;
         margin-top: -191px;
-        background: url(../assets/login_auth.png);
+        background: url(../assets/login_modal.png);
         border-radius: 5px;
-        box-shadow: 1px 1px 5px #333333, -1px -1px 5px #333333;
+        box-shadow: 2px 2px 5px #666, -2px -2px 5px #666;
     }
-
-    .login-form {
-        margin-left: 206px;
-        width: 380px;
-        height: 322px;
-        padding: 30px 40px;
-    }
-
-    .login-form-title {
-        height: 70px;
-        text-align: center;
-        letter-spacing: 5px;
-        text-shadow: 1px 1px 3px #666;
-        color: #197BAB;
-        font-size: 24px;
-    }
-
 </style>
