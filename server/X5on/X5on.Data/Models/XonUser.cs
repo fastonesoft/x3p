48ad14f7f6005a51452e8e37b1d027fc033baeaa
fastonesoft <2347632@qq.com>
2019-10-16 14:18:34 +0800

    change.server

---
+++ server/X5on/X5on.Data/Models/XonUser.cs
@@ -0,0 +1,9 @@
+﻿namespace X5on.Data.Models
+{
+    public class XonUser
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Pass { get; set; }
+    }
+}
